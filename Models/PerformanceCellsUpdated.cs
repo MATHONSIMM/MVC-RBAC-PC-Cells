aa858e85ef41cb4999addecd9e9705716d21e32d
Mphikeleli Mbongiseni Mathonsi <mphikelelimm@gmail.com>
2023-11-20 14:48:19 +0200

    Add project files.

---
+++ Models/PerformanceCellsUpdated.cs
@@ -0,0 +1,108 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace MappingList.Models
+{
+    public class PerformanceCellsUpdated
+    {
+        [Required]
+        [StringLength(80)]
+        [Unicode(false)]
+        public string? PerformanceCell { get; set; }
+
+
+        [StringLength(80)]
+        [Unicode(false)]
+        public string? SubPerformanceCell { get; set; }
+
+
+        [StringLength(80)]
+        [Unicode(false)]
+        public string? SubSubPerformanceCell { get; set; }
+
+
+        [StringLength(4)]
+        [Unicode(false)]
+        public string? Division { get; set; }
+
+
+        [StringLength(4)]
+        [Unicode(false)]
+        public string? Subdivision { get; set; }
+
+
+        [StringLength(4)]
+        [Unicode(false)]
+        public string? Department { get; set; }
+
+
+        [StringLength(4)]
+        [Unicode(false)]
+        public string? Partner { get; set; }
+
+
+        [StringLength(4)]
+        [Unicode(false)]
+        public string? Class { get; set; }
+
+
+        [StringLength(10)]
+        [Unicode(false)]
+        public string? Scheme { get; set; }
+
+
+        [StringLength(4)]
+        [Unicode(false)]
+        public string? Company { get; set; }
+
+
+        [StringLength(20)]
+        [Unicode(false)]
+        public string? BusinessUnit { get; set; }
+
+
+        [StringLength(4)]
+        [Unicode(false)]
+        public string? Years { get; set; }
+
+
+        [Column(TypeName = "numeric(18, 2)")]
+        public decimal? Splits { get; set; }
+
+
+        [Key]
+        [Column("PCKey")]
+        public int Pckey { get; set; }
+
+
+        [Column(TypeName = "datetime")]
+        public DateTime? ModifiedDate { get; set; }
+
+
+        [StringLength(20)]
+        [Unicode(false)]
+        public string? ModifiedUser { get; set; }
+
+        [StringLength(10)]
+        [Unicode(false)]
+        public string? SubDivisionExclude { get; set; }
+
+        [StringLength(10)]
+        [Unicode(false)]
+        public string? DepartmentExclude { get; set; }
+
+        [StringLength(10)]
+        [Unicode(false)]
+        public string? PartnerExclude { get; set; }
+
+        [StringLength(10)]
+        [Unicode(false)]
+        public string? ClassExclude { get; set; }
+
+        [StringLength(10)]
+        [Unicode(false)]
+        public string? SchemeExclude { get; set; }
+    }
+}
